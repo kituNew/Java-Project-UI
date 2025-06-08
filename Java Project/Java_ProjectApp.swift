ff8fbed3a26a0c6e75183e8a56e6bfb52bacd6e4
kituNew <vlad640vlad@gmail.com>
2025-06-08 11:46:20 +0300

    Initial Commit

---
+++ Java Project/Java_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Java_ProjectApp.swift
+//  Java Project
+//
+//  Created by Zaitsev Vladislav on 08.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct Java_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
